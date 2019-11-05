613d0d44691df224d5cf858944d1a64128b005c5
Or Mendel <57266329+Ormendel@users.noreply.github.com>
2019-11-05 15:54:13 +0200

    Updating Polynom.Java
    שים לב שהוספתי שיטת מיון ושיטת הדפסה של הפולינום (כמובן ממוין לאחר חיבור המונומים)
    רק לגבי השורש אני לא בטוח, זה נטו לפי חשבון אינפי 1.
    אם תמצא דרך יותר טובה אז תגיד לי ונתקן

--- Polynom.java
+++ Polynom.java
@@ -1,289 +1,265 @@
 package myMath;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.function.Predicate;
-
+import java.util.*;
 import myMath.Monom;
 
 /**
- * This class represents a Polynom with add, multiply functionality, it also
- * should support the following: 1. Riemann's Integral:
- * https://en.wikipedia.org/wiki/Riemann_integral 2. Finding a numerical value
- * between two values (currently support root only f(x)=0). 3. Derivative
+ * This class represents a Polynom with add, multiply functionality, it also should support the following:
+ * 1. Riemann's Integral: https://en.wikipedia.org/wiki/Riemann_integral
+ * 2. Finding a numerical value between two values (currently support root only f(x)=0).
+ * 3. Derivative
  * 
  * @author Boaz
  *
  */
-public class Polynom implements Polynom_able {
 
-	private ArrayList<Monom> p = new ArrayList<Monom>();
+public class Polynom implements Polynom_able
+{
 
 	/**
 	 * Zero (empty polynom)
 	 */
-	public Polynom() {
-		this.add(new Monom(0, 0));
+	private static final Monom_Comperator comp=new Monom_Comperator();
+	private ArrayList<Monom>list;
+	public Polynom() 
+	{
+		list=new ArrayList<Monom>();
 	}
-
 	/**
-	 * init a Polynom from a String such as: {"x", "3+1.4X^3-34x",
-	 * "(2x^2-4)*(-1.2x-7.1)", "(3-3.4x+1)*((3.1x-1.2)-(3X^2-3.1))"};
-	 * 
+	 * init a Polynom from a String such as:
+	 *  {"x", "3+1.4X^3-34x", "(2x^2-4)*(-1.2x-7.1)", "(3-3.4x+1)*((3.1x-1.2)-(3X^2-3.1))"};
 	 * @param s: is a string represents a Polynom
 	 */
-	public Polynom(String s) {
-		try {
+	
+	/**
+	 * Constructor to build polynom from a given string
+	 * @param s - represents a valid polynom
+	 */
+	public Polynom(String s)
+	{
+		try 
+		{
+			s = s.replaceAll("\\-", "+-"); // fix the '-' problem for a_1x^b_1-a_2x^b_2
 			s = s.replaceAll(" ", "");
-			s = s.replaceAll("\\+", " +");
-			s = s.replaceAll("\\-", " -");
-
-			if (s.charAt(0) == ' ')
-				s = s.substring(1);
-
-			String[] S = s.split(" ");
-
-			for (int i = 0; i < S.length; i++) {
-				String tmp = "";
-
-				if (S[i].length() == 1) {
-
-					if (S[i].charAt(0) == '+' || S[i].charAt(0) == '-') {
-						i++;
-					}
-
+			String[] split = s.split("[+]| ");
+			for(int i=0; i<split.length; i++)
+			{
+				if(!split[i].isEmpty())
+				{
+					Monom mon = new Monom(split[i]);
+					add(mon);
 				}
-
-				tmp = S[i];
-
-				if (tmp.charAt(0) == 'x')
-					tmp = "1" + tmp;
-
-				if (tmp.charAt(0) == '+' && tmp.charAt(1) == 'x')
-					tmp = "1" + tmp.substring(1);
-
-				if (tmp.charAt(0) == '-' && tmp.charAt(1) == 'x')
-					tmp = "-1" + tmp.substring(1);
-
-				if (tmp.charAt(tmp.length() - 1) == 'x')
-					tmp += "^1";
-
-				if (!(tmp.contains("x"))) {
-					double a = Double.parseDouble(tmp);
-					int b = 0;
-					this.p.add(new Monom(a, b));
-					return;
-				}
-
-				String[] m = tmp.split("x\\^");
-				double a = 0;
-				int b = 0;
-
-				a = Double.parseDouble(m[0]);
-				b = Integer.parseInt(m[1]);
-
-				this.p.add(new Monom(a, b));
 			}
 		}
-
-		catch (Exception error) {
-			System.err.println("Please insert polyonym: ax^b (a,b parameters)");
+		catch(Exception E)
+		{ 
+			throw new RuntimeException("The string "+s+" contains illegal characters!");
 		}
-
+	}
+	@Override
+	public double f(double x)
+	{
+		double sum = 0;
+		Iterator<Monom> it = list.iterator();
+		while(it.hasNext())
+		{
+			Monom temp = it.next();
+			sum+= temp.f(x);
+		}
+		return sum;
 	}
 
 	@Override
-	public double f(double x) {
-		double ans = 0;
-		Iterator<Monom> iter = this.p.iterator();
-		while (iter.hasNext()) {
-			Monom tmp = new Monom(iter.next());
-			ans += tmp.f(x);
+	public void add(Polynom_able p1)
+	{
+		Iterator<Monom> it = p1.iteretor();
+		while(it.hasNext())
+		{
+			Monom temp = new Monom(it.next());
+			this.add(temp);
+		}
+	}
+
+	@Override
+	public void add(Monom m1)
+	{
+		if(list.size()!=0)
+		{
+			boolean hasNewPower = true;
+			Iterator<Monom> it = list.iterator();
+			while(it.hasNext())
+			{
+				Monom temp = it.next();
+				if(m1.get_power() == temp.get_power())
+				{
+					if(m1.get_coefficient() + temp.get_coefficient()==0)
+						it.remove(); // if its from the form 0*x then remove it from the list
+					else 
+						temp.add(m1);
+					hasNewPower = false;
+				}
+			}
+			if(hasNewPower && m1.get_coefficient()!=0) 
+				list.add(m1);
+		}
+		else
+			if(m1.get_coefficient() != 0) 
+				list.add(m1); //if its from the form 0*x^n when n in N then no need to add zero, so it's ok for the list to remain empty
+	}
+
+	@Override
+	/**
+	 * Substract takes pair of monom and change the coefficient to be MINUS the contemporary coefficient
+	 */
+	public void substract(Polynom_able p1)
+	{
+		Iterator<Monom> it = p1.iteretor();
+		while(it.hasNext())
+		{
+			Monom temp = it.next();
+			double newcoef = -(temp.get_coefficient());
+			Monom newmonom = new Monom(newcoef,temp.get_power());
+			add(newmonom);
+		}
+		//When the loop ends the upper polynom will be substracted by Polynom_able p1
+	}
+
+	@Override
+	public void multiply(Polynom_able p1)
+	{
+		Polynom c = new Polynom();//This is the new object we would like to update to the list
+		Iterator<Monom> input_It = p1.iteretor();
+		while(input_It.hasNext())
+		{
+			Monom inputMonom = input_It.next();
+			for(int i=0; i<list.size();i++)
+			{
+				Monom p1_temp_monom = new Monom(inputMonom);
+				p1_temp_monom.multiply(list.get(i));
+				c.add(p1_temp_monom);
+			}
+		}
+		list.clear();
+		add(c);//Finish
+	}
+	@Override
+	public void multiply(Monom m1)
+	{
+		for(int i=0;i<list.size();i++)
+			list.get(i).multiply(m1);
+	}
+	@Override
+	public boolean equals(Polynom_able p1)
+	{
+		String s1 = p1.toString();
+		String s2 = toString();
+		return s1.equals(s2);
+	}
+
+	@Override
+	public boolean isZero()
+	{
+		return list.size()==0;
+	}
+
+	@Override
+	public double root(double x0, double x1, double eps)
+	{
+		//Infitisimaly 1
+		if(f(x0)*f(x1)>0) 
+		{
+			throw new RuntimeException("Wrong input, f(x0)*f(x1)>0");
+		}
+		else
+		{
+			double middle = (x1 + x0) / 2; 
+			while(Math.abs(x1 - x0) >= eps)
+			{
+				middle = (x1 + x0) / 2;
+				if(f(middle)==0) return middle;
+				else if(f(middle)*f(x0) < 0 ) //root lies between x0 and middle. So we recur for x0 and middle
+					x1 = middle;
+				else //root lies between middle and x1. So we recur for middle and x1
+					x0 = middle;
+			}
+			return middle; 
+		}
+	}
+
+	@Override
+	public Polynom_able copy() 
+	{
+		Polynom pol = new Polynom();
+		Iterator<Monom> it = list.iterator();
+		while(it.hasNext())
+		{
+			Monom temp = it.next();
+			pol.add(temp);
+		}
+		return pol;
+	}
+
+	@Override
+	public Polynom_able derivative() 
+	{
+		Polynom pol = new Polynom();
+		Iterator<Monom> it = list.iterator();
+		while(it.hasNext())
+		{
+			Monom mon = new Monom(it.next().derivative());
+			pol.add(mon);
+		}
+		return pol;
+	}
+
+	@Override
+	public double area(double x0, double x1, double eps)
+	{
+		double ans=0;
+		for(double i=x0;i<=x1;i+=eps)
+		{
+			ans+=f(i);
 		}
 		return ans;
 	}
 
 	@Override
-	public void add(Polynom_able p1) {
-		Iterator<Monom> iter = p1.iteretor();
-		while (iter.hasNext()) {
-			p.add(new Monom(iter.next()));
-		}
-
+	public Iterator<Monom> iteretor()
+	{
+		return list.iterator();	
 	}
-
-	@Override
-	// not working  if p.cofficient = 1.
-	public void add(Monom m1) {
-		try {
-
-			int i = 0;
-			boolean flag = false;
-			Iterator<Monom> iter = p.iterator();
-			double a = 0;
-			int b = 0;
-
-			while (iter.hasNext()) {
-				if (iter.next().get_power() == m1.get_power()) {
-					a = iter.next().get_coefficient() + m1.get_coefficient();
-					b = m1.get_power();
-					Monom m2 = new Monom(a, b);
-					p.set(i, m2);
-					flag = true;
-				}
-				i++;
-			}
-
-			if (!flag)
-				p.add(m1);
-		}
-
-		catch (Exception error) {
-			System.err.println("please add Monom: ax^b (a,b parameters)");
-		}
-
-	}
-
-	@Override
-	public void substract(Polynom_able p1) {
-		Iterator<Monom> iter = p1.iteretor();
-		while (iter.hasNext()) {
-			Monom tmp = new Monom(iter.next().get_coefficient(), iter.next().get_power() * -1);
-			p.add(tmp);
-		}
-
-	}
-
-	@Override
-	public void multiply(Polynom_able p1) {
-		Iterator<Monom> iter = p1.iteretor();
-		Iterator<Monom> iter2 = p.iterator();
-		Polynom New = new Polynom();
-		while(iter2.hasNext()) {
-			while(iter.hasNext()) {
-				double a = iter2.next().get_coefficient()*iter.next().get_coefficient();
-				int b = iter2.next().get_power() + iter.next().get_power();
-				Monom tmp = new Monom(a,b);
-				New.add(tmp);
-			}
-		}
-		this.p = New.p;
-	}
-
-	@Override
-	public boolean equals(Polynom_able p1) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isZero() {
-		for(int i = 0; i < p.size(); i++) {
-			if(p.get(i).get_coefficient() !=0)
-				return false;
-		}
-		return true;
-	}
-
-	@Override
-	public double root(double x0, double x1, double eps) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Polynom_able copy() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Polynom_able derivative() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public double area(double x0, double x1, double eps) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Iterator<Monom> iteretor() {
-		return p.iterator();
-	}
-
-	@Override
-	public void multiply(Monom m1) {
-		// TODO Auto-generated method stub
-
+	/**
+	 * This function is responsible for sorting the polynom by power
+	 */
+	private void sort()
+	{
+		list.sort(comp);	
 	}
 	
-	public String toString() {
-		//return for zero polynom
-				if (isZero() == true)
-					return "0x^0";
-
-				String forPrint = "";
-				Iterator<Monom> it = p.iterator();
-				boolean flagNotFirst = false;
-				//first monom in polynom
-				if (it.hasNext()) {		
-					Monom mPrintFirst = new Monom(it.next());
-					if (mPrintFirst.get_coefficient() == 0)
-						forPrint+="";
-					else {
-						flagNotFirst = true;
-						forPrint+= mPrintFirst;
-					}
-				}
-
-				while (it.hasNext()) {
-
-					Monom mPrint = new Monom (it.next());
-					if (mPrint.get_coefficient() == 0)
-						continue;
-
-					if (mPrint.get_coefficient() > 0)
-					{
-						if (flagNotFirst)	
-							forPrint+=" + ";
-						else
-							flagNotFirst = true;	//not first anymore
-					}
-
+	public String toString() 
+	{
+		if(list.size() != 0)
+		{
+			String s = "";
+			sort();
+			for(int i=0; i<list.size();i++)
+			{
+				boolean neg = false;
+				if(list.get(i).get_coefficient() < 0 )
+					neg = true;
+				if(!neg)
+				{  
+					if(i==0)
+						s+= list.get(i); 
 					else
-					{
-						mPrint.set_coefficient(-mPrint.get_coefficient());
-						if (flagNotFirst)
-							forPrint+=" - ";
-						else
-						{
-							forPrint+="-";
-							flagNotFirst = true;	//not first anymore
-						}	
-					}
-
-					forPrint+=mPrint.toString();
-
+						s+= "+"+list.get(i);
 				}
-				return forPrint;
+				else //its negative
+					s+= list.get(i);
+			}
+			return s;
+		}
+		else
+			return "0"; 
 	}
-
-	public static void main(String[] args) {
-		Polynom m = new Polynom("0");
-		m.isZero();
-		Polynom m2 = new Polynom("6x^5+x^4+2");
-		//m.multiply(m2);
-		String s = m.toString();
-		double fx = m.f(2);
-		Monom m1 = new Monom(3, 5);
-		m.add(m1);
-		//Polynom_able p1 = null;
-		//m.substract(p1);
-		m = new Polynom(s);
-		System.out.println(2 + ") " + m + "    \tisZero: " + m.isZero() + "\t f(" + 2 + ") = " + fx);
-	}
-
 }
