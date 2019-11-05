860d3b226665a91338369a1eec97501e4f08eb0c
Or Mendel <57266329+Ormendel@users.noreply.github.com>
2019-11-05 15:44:24 +0200

    Update Monom_Comperator.java

--- Monom_Comperator.java
+++ Monom_Comperator.java
@@ -2,32 +2,11 @@ package myMath;
 
 import java.util.Comparator;
 
-public class Monom_Comperator implements Comparator<Monom> {
+public class Monom_Comperator implements Comparator<Monom> 
+{
 
-	public Monom_Comperator() {;}
-	public int compare(Monom o1, Monom o2) {
-		int dp = o2.get_power() - o1.get_power();
-		return dp;
+	public int compare(Monom m1, Monom m2)
+	{
+		return (m2.get_power() - m1.get_power());
 	}
-
-	// ******** add your code below *********
-	
-	public Double compare2(Monom o1, Monom o2) {
-		Double dc = o2.get_coefficient() - o1.get_coefficient();
-		return dc;
-	}
-	
-	public boolean compare3 (Monom m1 , Monom m2) {
-		if (compare(m1,m2) == 0 && compare2(m1,m2) == 0) {
-			return true;
-		}
-		return false;
-	}
-
-	
-	
-	
-	
-
 }
-
