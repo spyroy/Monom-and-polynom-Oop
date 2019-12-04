d828cb028653cfe451f98812dca161aa7df522e7
Or Mendel <57266329+Ormendel@users.noreply.github.com>
2019-12-04 13:04:41 +0200

    Implemeting Or's part
    TODO: constructor that gets a string.

--- Ex1/src/Ex1/ComplexFunction.java
+++ Ex1/src/Ex1/ComplexFunction.java
@@ -6,108 +6,187 @@ import java.util.LinkedList;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
-public class ComplexFunction implements complex_function  {
+public class ComplexFunction implements complex_function  
+{
 	
-	class Node{
-		Operation o;
-		function f;
-		
-		public Node() 
-	    { 
-	        this.o = Operation.None;
-	        this.f = null;
-	    } 
-		
-		public Node(function f1 , Operation o) 
-	    { 
-	        this.o = o;
-	        this.f = f1;
-	    } 
-	}
-	
-	
-	
-	private LinkedList<Node> cfl = new LinkedList<>();
-	
-	public ComplexFunction(function f1, Operation o, function f2) {
-		Node cf = new Node(f1, o);
-		cfl.add(cf);
-		Node add = new Node(f2,Operation.None);
-		cfl.add(add);
-	}
-	
-	public ComplexFunction(function f1) {
-		if(f1 instanceof ComplexFunction) {
-			Node left = new Node(((ComplexFunction) f1).left(),((ComplexFunction) f1).getOp());
-			Node right = new Node(((ComplexFunction) f1).right(),((ComplexFunction) f1).getOp());
-			cfl.add(left);
-			cfl.add(right);
-		}
-		else if(f1 instanceof Polynom) {
-			Node n = new Node (f1,Operation.None);
-			cfl.add(n);
-		}
-		else if(f1 instanceof Monom) {
-			Node n = new Node (f1,Operation.None);
-			cfl.add(n);
-		}
-			
-	}
-	
+	private function left,right;
+	private Operation o;
 
-	private Iterator<Node> iteretor() {
-		return this.cfl.iterator();
+	/***********************************Getters*******************************/
+	public function getLeft() 
+	{
+		return left;
 	}
 	
-	public void plus(function f1) {
-		ComplexFunction cf= new ComplexFunction(f1);
-		
-		//Node cf = new Node(f1,Operation.Plus);
-////		cf.left = this;
-////		cf.o = Operation.Plus;
-////		cf.right = f1;
-////		cfl.clear();
-		//cfl.add(cf);
-////		o.o = Operation.Plus;
-////		Node_f2 right = new Node_f2();
-////		//Object iter = new Node_f2();
-////		right.f2 = f1;
-//////		Iterator<Object> listiter = iteretor();
-//////		while(cfl.iterator().hasNext()) {
-//////			iter = listiter.next();
-//////		}
-////		cfl.add(o);
-////		cfl.add(right);
-//		Node n = new Node();
-//		Node Pointer = new Node();
-//		n.f = f1;
-//		Iterator<Node> functionIter = iteretor();
-//		while (this.cfl.iterator().hasNext()){
-//			Pointer=functionIter .next();
-//		}
-//		Pointer.o=Operation.Plus;
-//		this.cfl.add(n);
-			
+	public function getRight() 
+	{
+		return right;
 	}
-	public void mul(function f1) {;}
-	public void div(function f1) {;}
-	public void max(function f1) {;}
-	public void min(function f1) {;}
-	public void comp(function f1) {;}
-	public function left() {return null;}
-	public function right(){return cfl.getLast().f;}
-	public Operation getOp(){
-		Node iter = new Node();
-		Iterator<Node> liter = iteretor();
-		while (iteretor().next().o != Operation.None){
-			iter = liter.next();
+	public Operation getOp() 
+	{
+		return o;
+	}
+	/***********************************Setters*******************************/
+	public void setLeft(function left) 
+	{
+		this.left=left;
+	}
+	
+	public void setRight(function right) 
+	{
+		this.right=right;
+	}
+	
+	public void setOP(Operation o) 
+	{
+		this.o=o;
+	}
+	/*--------------------------------------------------------------*/
+	
+	/*****************************Constructors*******************************/
+	public ComplexFunction (function f) 
+	{
+		//In this case the program gets only one function with no operator, so we need to initialize it to be left.
+		left=f;
+		o = Operation.None;
+	}
+	public ComplexFunction(function left, Operation o, function right) 
+	{
+		this.left=left;
+		this.o=o;
+		if(right!=null)
+			this.right=right;
+	}
+	public ComplexFunction(function left, String o, function right) 
+	{
+		this.left=left;
+		if(right!=null)
+			this.right=right;
+		switch(o.toUpperCase())
+		{
+		case "PLUS"  :this.o = Operation.Plus; 	break;
+		case "TIMES" :
+		case "MUL":
+		case "MULT":
+			this.o = Operation.Times; break;
+		case "DIVID" :this.o = Operation.Divid; break;
+		case "MAX"   :this.o = Operation.Max; 	break;
+		case "MIN"   :this.o = Operation.Min; 	break;
+		case "COMP"  :this.o = Operation.Comp; 	break;
+		case "NONE"  :this.o = Operation.None; 	break;
+		default      :this.o = Operation.Error;	break;
 		}
-		return iter.o;
 	}
-	public double f(double x) {return x;}
-	public function initFromString(String s){return null;}
-	public function copy() {return null;}
+
+	public ComplexFunction(String s)
+	{	//Assuming the given string is valid
+		
+		//TODO 
+		function check = initFromString(s);
+		ComplexFunction cf = (ComplexFunction)check;
+		this.left = cf.left();
+		this.right = cf.right();
+		this.o = cf.getOp();
+		
+	}
+	/*******************************************************************************/
+	/*****************************END OF CONSTRUCTORS*******************************/
+	/*******************************************************************************/
+	public boolean isZero(function fRight)//Useful function for checking if right side of function equals to the ZERO function
+	{
+		if(fRight==null)
+			return false;
+		if(fRight instanceof Polynom)
+		{
+			Polynom p=new Polynom();
+			if(p.equals((Polynom)fRight))
+					return true;
+		}
+		else if(fRight instanceof Monom)
+		{
+			Monom m=new Monom();
+			if(m.equals((Monom)fRight))
+					return true;
+		}
+		return false;
+	}
+	/****************Using different operators for determine the situation in the tree*****************************/
+	public void plus(function f1)
+	{
+		o=Operation.Plus;
+		//Assuming there is a function left, we need to add (plus) function right
+		if ( right != null )
+		{
+			function fLeft =new ComplexFunction(left, o,right);
+			left = fLeft;
+		}
+		right=f1;
+	}
+	public void mul(function f1)
+	{
+		o=Operation.Times;
+		//Assuming there is a function left, we need to mul (times) function right
+		if ( right != null )
+		{
+			function fLeft =new ComplexFunction(left, o,right);
+			left = fLeft;
+		}
+		right=f1;
+	}
+	public void div(function f1) 
+	{
+		//TODO exception when f1 represents the ZERO function f(x)=0 (need to be checked by JUNIT tests)
+		o=Operation.Divid;
+		//Assuming there is a function left, we need to mul (times) function right
+		if ( right != null )
+		{
+			function fLeft =new ComplexFunction(left, o,right);
+			left = fLeft;
+		}
+		right=f1;
+	}
+	public void max(function f1) 
+	{
+		o=Operation.Max;
+		//Assuming there is a function left, we need to mul (times) function right
+		if ( right != null )
+		{
+			function fLeft =new ComplexFunction(left, o,right);
+			left = fLeft;
+		}
+		right=f1;
+	}
+	public void min(function f1)
+	{
+		o=Operation.Min;
+		//Assuming there is a function left, we need to mul (times) function right
+		if ( right != null )
+		{
+			function fLeft =new ComplexFunction(left, o,right);
+			left = fLeft;
+		}
+		right=f1;
+	}
+	public void comp(function f1) 
+	{
+		o=Operation.Comp;
+		if ( right != null )
+		{
+			function fLeft =new ComplexFunction(left, o,right);
+			left = fLeft;
+		}
+		right=f1;
+	}
 	
+	/*--------------------------------------------------------------*/
+	public double f(double x) {return x;}
+	
+	
+	//*****************Matan's part*********************************
+	public function initFromString(String s){return null;}
+	public function copy() 
+	{return null;}
+
 	@Override
 	public String toString() {
 		Iterator<Node> listiter = iteretor();
@@ -125,6 +204,19 @@ public class ComplexFunction implements complex_function  {
 		s = s.replaceAll("Null", "");
 		return s;
 	}
-	
+
+	@Override
+	public function left() 
+	{
+		return left;
+	}
+
+	@Override
+	public function right()
+	{
+		return right;
+	}
+
+
 
 }
