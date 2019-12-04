ef70eca2021e9e2f17d5f8a4e0e4e1bf13c4cd76
matan <57196433+spyroy@users.noreply.github.com>
2019-12-04 22:23:16 +0200

    fix init from string!!

--- Ex1/src/Ex1/ComplexFunction.java
+++ Ex1/src/Ex1/ComplexFunction.java
@@ -44,19 +44,33 @@ public class ComplexFunction implements complex_function
 	/*--------------------------------------------------------------*/
 	
 	/*****************************Constructors*******************************/
+	
+	// default constructor
+	public ComplexFunction () {
+		left = new Polynom();
+		o = Operation .None;
+		right = null;
+	}
+	
 	public ComplexFunction (function f) 
 	{
 		//In this case the program gets only one function with no operator, so we need to initialize it to be left.
 		left=f;
 		o = Operation.None;
 	}
+	
 	public ComplexFunction(function left, Operation o, function right) 
 	{
 		this.left=left;
 		this.o=o;
 		if(right!=null)
 			this.right=right;
+		else {
+			this.right = null;
+			this.o = Operation.None;
+		}
 	}
+	
 	public ComplexFunction(function left, String o, function right) 
 	{
 		this.left=left;
@@ -89,9 +103,11 @@ public class ComplexFunction implements complex_function
 		this.o = cf.getOp();
 		
 	}
+	
 	/*******************************************************************************/
 	/*****************************END OF CONSTRUCTORS*******************************/
 	/*******************************************************************************/
+	
 	public boolean isZero(function fRight)//Useful function for checking if right side of function equals to the ZERO function
 	{
 		if(fRight==null)
@@ -110,7 +126,9 @@ public class ComplexFunction implements complex_function
 		}
 		return false;
 	}
+	
 	/****************Using different operators for determine the situation in the tree*****************************/
+	
 	public void plus(function f1)
 	{
 		o=Operation.Plus;
@@ -122,6 +140,7 @@ public class ComplexFunction implements complex_function
 		}
 		right=f1;
 	}
+	
 	public void mul(function f1)
 	{
 		o=Operation.Times;
@@ -133,6 +152,7 @@ public class ComplexFunction implements complex_function
 		}
 		right=f1;
 	}
+	
 	public void div(function f1) 
 	{
 		//TODO exception when f1 represents the ZERO function f(x)=0 (need to be checked by JUNIT tests)
@@ -145,6 +165,7 @@ public class ComplexFunction implements complex_function
 		}
 		right=f1;
 	}
+	
 	public void max(function f1) 
 	{
 		o=Operation.Max;
@@ -156,6 +177,7 @@ public class ComplexFunction implements complex_function
 		}
 		right=f1;
 	}
+	
 	public void min(function f1)
 	{
 		o=Operation.Min;
@@ -167,6 +189,7 @@ public class ComplexFunction implements complex_function
 		}
 		right=f1;
 	}
+	
 	public void comp(function f1) 
 	{
 		o=Operation.Comp;
@@ -177,33 +200,130 @@ public class ComplexFunction implements complex_function
 		}
 		right=f1;
 	}
-	
-	/*--------------------------------------------------------------*/
-	public double f(double x) {return x;}
-	
-	
 	//*****************Matan's part*********************************
-	public function initFromString(String s){return null;}
+	/*--------------------------------------------------------------*/
+	// if the left/right is Monom/Polynom/ComplexFunction we use f(Monom/Polynom/ComplexFunction)
+	public double f(double x) 
+	{
+		switch(o)
+		{
+		case Plus  :return left.f(x) + right.f(x);
+		case Times : return left.f(x) * right.f(x);
+		case Divid :
+			if(right.f(x) != 0)
+				return left.f(x) / right.f(x);
+			System.err.println("cannot divide by zero");
+		case Max   :
+			double res = left.f(x) - right.f(x);
+			if(res<0)
+				return right.f(x);
+			return left.f(x);
+		case Min   :
+			double Res = left.f(x) - right.f(x);
+			if(Res<0)
+				return left.f(x);
+			return right.f(x);
+		case Comp  :return left.f(right.f(x));
+		case None  :return left.f(x);
+		default      :throw new RuntimeException("not a good operation");
+		}
+	}
+	
+	
+	
+	
+	// String of form: "Operation"("Polynom","Polynom") , (recursive)
+	// fix!!
+	public function initFromString(String s) {
+		if (!s.contains("(") && !s.contains(")")) {
+			this.left= new Polynom(s);
+			this.right=null;
+			this.o= Operation.None;
+			return this.left.initFromString(s);
+		}
+		s = s.toUpperCase();
+		s = s.replaceAll("X", "x");
+		String op = "";
+		if (s.charAt(0) == 'P' || s.charAt(0) == 'T' || s.charAt(0) == 'M' || s.charAt(0) == 'D' || s.charAt(0) == 'C'
+				|| s.charAt(0) == 'N')
+			op = new String(s.substring(0, s.indexOf('(')));
+		s = s.substring(s.indexOf('(') + 1);
+		s = s.substring(0, s.length()-1);
+		Operation o = Operation.None;
+		switch (op) {
+		case "PLUS":
+			o = Operation.Plus;
+			break;
+		case "TIMES":
+		case "MUL":
+		case "MULT":
+			this.o = Operation.Times;
+			break;
+		case "DIVID":
+			o = Operation.Divid;
+			break;
+		case "MAX":
+			o = Operation.Max;
+			break;
+		case "MIN":
+			o = Operation.Min;
+			break;
+		case "COMP":
+			o = Operation.Comp;
+			break;
+		case "NONE":
+			o = Operation.None;
+			break;
+		default:
+			o = Operation.Error;
+			break;
+		}
+
+//		if (o == Operation.Error) {
+//			if (s.contains(",")) {
+//				s = s.replaceAll(",", "");
+//				Polynom polL = new Polynom(s);
+//				this.left = polL;
+//				return this.left.initFromString(s);
+//			} else {
+//				Polynom polR = new Polynom(s);
+//				this.right = polR;
+//				return this.right.initFromString(s);
+//			}
+//		}
+
+		function nleft = initFromString(s.substring(0, s.indexOf(',') + 1));
+		function nright = initFromString(s.substring(s.indexOf(',') + 1));
+		function f = new ComplexFunction(nleft, o, nright);
+
+		return f;
+
+	}
 	public function copy() 
-	{return null;}
+	{
+	ComplexFunction  cf = new ComplexFunction(left,o, right);
+	return cf;
+	}
+	
+	// this function will work only in specific range
+	public boolean equals(Object obj) {
+		return false;
+	}
 
 	@Override
 	public String toString() {
-		Iterator<Node> listiter = iteretor();
-		Node iter = new Node();
-		String s = "";
-		while(listiter.hasNext()) {
-			iter = listiter.next();
-			s+=iter.o;
-			s+=iter.f;
-			s+=iter.o;
-			//cfl.remove(iter);
+		switch(o) {
+		case Plus  :return "Plus(" + this.right.toString() + "," + this.left.toString();
+		case Times : return "Times(" + this.right.toString() + "," + this.left.toString();
+		case Divid :return "Divid(" + this.right.toString() + "," + this.left.toString();
+		case Max   :return "Max(" + this.right.toString() + "," + this.left.toString();
+		case Min   :return "Min(" + this.right.toString() + "," + this.left.toString();
+		case Comp  :return "Comp(" + this.right.toString() + "," + this.left.toString();
+		case None  :return "None(" + this.right.toString() + "," + this.left.toString();
+		default      :throw new RuntimeException("not a good operation");
 		}
-		s = s.replaceAll("Plus","+");
-		s = s.replaceAll("None","");
-		s = s.replaceAll("Null", "");
-		return s;
 	}
+
 
 	@Override
 	public function left() 
@@ -215,6 +335,12 @@ public class ComplexFunction implements complex_function
 	public function right()
 	{
 		return right;
+	}
+	
+	public static void main(String[] args) {
+		function cf = new Monom();
+		cf = cf.initFromString("x^3");
+		System.out.println(cf.toString());
 	}
 
 
